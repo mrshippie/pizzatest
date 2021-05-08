7e6d0ba0b56b5aa744c46eb0ae37cef557f0660f
mrshippie <36476165+mrshippie@users.noreply.github.com>
2021-05-08 17:24:21 +0200

    Add files via upload

---
+++ pizza2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="pizza2" tilewidth="32" tileheight="32" tilecount="361" columns="19">
+ <image source="../Documents/Spieleberatung/material-spieleberatung/pixeln-WA/pizza2.png" width="608" height="608"/>
+</tileset>
